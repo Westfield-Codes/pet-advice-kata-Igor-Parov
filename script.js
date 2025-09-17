56eff1b6e26c0c8c9453c75594273f9b6e5375f4
Igor-Parov <ihor207415@student-schoolsofwestfield.org>
2025-09-17 12:12:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 /* Pet Advice Kata */
 
 /* Var, Prompt, Confirm, Alert, Compound Conditional */
+var pet = "";
+var allergic = confirm("Are you allergic to fur?");
+var active = confirm("Do you want an active pet?");
+if (allergic == false && active == false) pet = " cat";
+else if (allergic == false && active == true) pet = " dog";
+else if (allergic == true && active == false) pet = " fish";
+else pet = " spider";
 
+alert ("try a" + pet);
 // Create pet variable, set equal to empty quotes ("")
 // Ask if they are allergic, store in a boolean variable with OK/Cancel
 // If they are allergic, ask if they want a cute pet, store in a boolean variable with OK/Cancel
